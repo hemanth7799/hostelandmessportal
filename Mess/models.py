abe0adad1c0c371341263f9262ffe8302612b624
root <root@localhost.localdomain>
2018-10-25 11:17:42 -0400

    commit models

--- Mess/models.py
+++ Mess/models.py
@@ -2,14 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 #from hostel.models import Student
 class MessFeedback(models.Model):
-	#studentrollno= ForeignKey(Student,on_delete=models.CASCADE)
+	roll_no= models.ForeignKey(Student,on_delete=models.CASCADE)
 	feedbackid = models.CharField(max_length=250)
 	time = models.CharField(max_length=250)
 	feedback = models.CharField(max_length=200)
 	def __str__(self):
 		return str(str(self.feedback) +","+str(self.time))
 class Refund(models.Model):
-	#studentrollno= ForeignKey(Student,on_delete=models.CASCADE)
+	roll_no= models.ForeignKey(Student,on_delete=models.CASCADE)
 	#employeeid= ForeignKey(Employee,on_delete=models.CASCADE)
 	refundid = models.CharField(max_length=250)
 	outdate=models.CharField(max_length=200)
