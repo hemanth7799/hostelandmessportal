5c2bfb21967c0ac69eff2e3bd072eede7b989cc3
root <root@localhost.localdomain>
2018-10-25 10:27:11 -0400

    commit2

---
+++ Mess/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import MessFeedback ,Refund
+admin.site.register(MessFeedback)
+
+admin.site.register(Refund)
+
+# Register your models here.
