6eb7186da885330171f3accfc554213d2c62beab
root <root@localhost.localdomain>
2018-10-25 15:44:30 -0400

    settings update

--- hostelandmess/settings.py
+++ hostelandmess/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'Hostel.apps.HostelConfig',
     'Mess.apps.MessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
