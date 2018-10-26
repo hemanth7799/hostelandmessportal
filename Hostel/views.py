c5b04fbc6257aec181749922b310f78f456caaea
venkatlokesh <venkat1999kumar@gmail.com>
2018-10-26 10:36:38 +0530

    viewsupdte

--- Hostel/views.py
+++ Hostel/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404,render
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 def inOutIndex(request):
 	form={}
   # if this is a POST request we need to process the form data
@@ -15,17 +16,45 @@ def inOutIndex(request):
 		out_time = request.POST['out_time']
 		out_place=request.POST['out_place']
 		out_reason=request.POST['out_reason']
-		
-		roomregistration=RoomRegistration()
-		roomregistration.in_time=in_time
-		roomregistration.out_time=out_time
-		roomregistration.out_place=out_place
-		roomregistration.out_reason=out_reason
-		roomregistration.save()
-		return HttpResponseRedirect('/dashboard/1')
+		usr=User.objects.get(username='lokesh')
+		stud=Student()
+		stud.user=usr
+		stud.rollno='S20160010078'
+		stud.save()
+		in_out=InOutList()
+		in_out.student=stud
+		in_out.in_time=in_time
+		in_out.out_time=out_time
+		in_out.out_place=out_place
+		in_out.out_reason=out_reason
+		in_out.save()
+		return HttpResponse('data stored in db')
 	context={'text':0}
 			
-	return render(request, 'rasp/add.html',context)
+	return render(request, 'Hostel/inout.html',context)
+def HostelComplaintIndex(request):
+	form={}
+  # if this is a POST request we need to process the form data
+	if request.method == 'POST':
+    # create a form instance and populate it with data from the request:
+		complain_time= request.POST['complain_time']
+		
+		complaint=request.POST['complaint']
+		usr=User.objects.get(username='lokesh')
+		stud=Student()
+		stud.user=usr
+		stud.rollno='S20160010078'
+		stud.save()
+		hos_compl=HostelComplaint()
+		hos_compl.complaint=complaint
+		hos_complain_time=complain_time
+
+		hos_compl.save()
+		return HttpResponse('data stored in db')
+	context={'text':0}
+			
+	return render(request, 'Hostel/hostelcomplaint.html',context)
+
 	
 
 
