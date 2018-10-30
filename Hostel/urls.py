32b48403bea1eee3cf7a69339915786579b61fd1
venkatlokesh <venkat1999kumar@gmail.com>
2018-10-30 20:15:48 +0530

    urlsupdate

--- Hostel/urls.py
+++ Hostel/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import include,url
 from . import views
 urlpatterns = [
 	url(r'^inout/$', views.inOutIndex ,name = "index"),
-	url(r'^hos_comp/$', views.HostelComplaintIndex ,name = "index")  ]
+	url(r'^hostel_complaint/$', views.HostelComplaintIndex ,name = "index1") ,
+	url(r'^guestEntry/$', views.GuestEntryIndex ,name = "index2") ]
