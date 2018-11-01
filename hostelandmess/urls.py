11c665213d5180abe4c1998779a3b9b1620722d5
root <root@localhost.localdomain>
2018-11-02 00:39:48 +0530

    call back

--- hostelandmess/urls.py
+++ hostelandmess/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mess/', include('Mess.urls'))
+    url(r'^login_user/(?P<token>.+)$',views.login,name="login"),
+    url(r'^mess/', include('Mess.urls')),
+    url(r'^hostel/', include('Hostel.urls'))
 ]
---
+++ hostelandmess/views.py
@@ -0,0 +1,14 @@
+import json
+import requests
+from django.shortcuts import get_object_or_404, render
+
+def login(request,token):
+    res = requests.post(url='https://serene-wildwood-35121.herokuapp.com/oauth/getDetails', data={
+    'token': token,
+    'secret': 'ff5bbfae032e66f2abdb15b113bfab5d3f1741b1ed1f60b04e7062d74024bad29f57b82e6cfe0eee53ee6e595405c00907af0179c85c2c0c396e6e3df1f250a7'
+})
+    res = json.loads(res.content)
+    email = res['student'][0]['Student_Email']
+    #print(email)
+    if(email):
+        return render(request,'hostel/login.html')
