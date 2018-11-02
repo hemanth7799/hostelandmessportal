ee9308ee4b1f244c06d029e05bfc18633074f1cc
root <root@localhost.localdomain>
2018-11-02 11:32:22 +0530

    login 1

--- Mess/urls.py
+++ Mess/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^feedback/$', views.feedback, name='feedback'), 
     url(r'^feedbacklist/$', views.feedbacklist, name='feedbacklist'),
     url(r'^refundlist/$', views.refundlist, name='refundlist'),
-    url('^login/(?P<token>.+)$',views.login_user,name="login_user")
+    url('^login/(?P<token>.+)$',views.login,name="login")
+    url('^$',views.login_user,name="login_user")
 ]
